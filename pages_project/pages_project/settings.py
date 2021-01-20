bff976d4ea2df22a7c27af24992b0a5e674c5c00
Dip Rawal <diprawal@hotmail.com>
2021-01-21 01:34:52 +0200

    Update settings.py

--- pages_project/pages_project/settings.py
+++ pages_project/pages_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g%xe@3!olkll@ps@lgk=aima1v4=pm9^)=dbqv4#n3_rk%#r-b'
+SECRET_KEY = 'keyhere'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
