51032a9678a8d4e8d85e71a98c47418a71461d3e
rawald <diprawal@hotmail.com>
2021-01-20 22:33:29 +0200

    Initial commit

---
+++ pages_project/pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import aboutPageView, homePageView
+
+urlpatterns = [
+    path('', homePageView.as_view(), name='home'),
+    path('about', aboutPageView.as_view(), name='about')
+]
